28260ea44706c075b5607e87f65ca18ffe640957
Admin <admin@Mac-Admin.local>
2015-07-30 21:53:18 +0300

    Initial Commit

---
+++ Tutorial_48/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tutorial_48
+//
+//  Created by Admin on 30.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
